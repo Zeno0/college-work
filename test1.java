6e28bdb27c413c8923a56fb6298c20bf0bd6d87b
Shekhar Kumar <zatch216@gmail.com>
2020-02-22 14:20:08 +0530

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,34 @@
+ import java.util.Scanner;
+
+
+public class test1{
+    char arr[] ;
+    int length;
+    // Constructor
+    public test(String x){
+        arr = new char[x.length()];
+    }
+    // calculate length method
+    public void calclenght(String x){
+        System.out.println("Here custom method is used. ");
+        for(int i=0; i<x.length();i++){
+            arr[i]= x.charAt(i);
+             }
+        System.out.println("Length of String "+x+" is: ");
+        for(int i=0; i<x.length();i++){
+            length++;
+            }
+        System.out.println(length);
+        } // close method
+    // main
+    public static void main (String args[]){
+   
+        System.out.println("please enter whatever you want in one line ");
+        Scanner scan = new Scanner(System.in);
+       String x = scan.nextLine(); 
+       test t =new test(x);
+        System.out.println("String entered by the user is "+x);
+        t.calclenght(x);  // method invoked
+        System.out.println("Lenght of String by using in-built function: "+x.length());
+    }
+}
