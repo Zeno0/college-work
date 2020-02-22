a16ba820acb2ffe076fcd5dfdbb19c7fa680e4e2
Shekhar Kumar <zatch216@gmail.com>
2020-02-22 14:16:54 +0530

    Update test0.java

--- test0.java
+++ test0.java
@@ -1,26 +1,23 @@
  import java.util.Scanner;
-
-
+ //Constructor
 public class test0{
     char arr[] ;
     public test(String x){
         arr = new char[x.length()];
     }
+   //display method
     public void display(String x){
         for(int i=0; i<x.length();i++){
             arr[i]= x.charAt(i);
-              
-        }
+              }
         System.out.println("Character Array is: ");
         for(int i=0; i<x.length();i++){
             System.out.printf("%s,",arr[i]);
-              
-        }
-        
-        
-    }
+              }
+       }  // method close
 
-    public static void main (String args[]){
+ // main   
+ public static void main (String args[]){
    
         System.out.println("please enter whatever you want in one line ");
         Scanner scan = new Scanner(System.in);
