ade29a2cd5537f767a75e35c57ede1c3dc0ac6a5
Shekhar Kumar <zatch216@gmail.com>
2020-02-22 14:14:55 +0530

    Create test0.java

---
+++ test0.java
@@ -0,0 +1,32 @@
+ import java.util.Scanner;
+
+
+public class test0{
+    char arr[] ;
+    public test(String x){
+        arr = new char[x.length()];
+    }
+    public void display(String x){
+        for(int i=0; i<x.length();i++){
+            arr[i]= x.charAt(i);
+              
+        }
+        System.out.println("Character Array is: ");
+        for(int i=0; i<x.length();i++){
+            System.out.printf("%s,",arr[i]);
+              
+        }
+        
+        
+    }
+
+    public static void main (String args[]){
+   
+        System.out.println("please enter whatever you want in one line ");
+        Scanner scan = new Scanner(System.in);
+       String x = scan.nextLine(); 
+       test t =new test(x);
+        System.out.println("String entered by the user is "+x);
+        t.display(x);
+    }
+}
